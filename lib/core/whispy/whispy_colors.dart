b3fa7bcc8ad867ae14d14f2aa3c3f495a0d59839
taejun <ptj01036251@gmail.com>
2025-06-08 23:07:50 +0900

    feat: 색 추가
    ColorSystem을 생성합니다
    - grayscale, error, success, black&white, main1&2로 구성되어있음

---
+++ lib/core/whispy/whispy_colors.dart
@@ -0,0 +1,54 @@
+import 'dart:ui';
+
+class WhispyColors {
+  //error
+  static Color error300 = const Color(0xffFF8881);
+  static Color error500 = const Color(0xffFF453A);
+  static Color error700 = const Color(0xffA32C25);
+
+  //success
+  static Color success300 = const Color(0xff81FF87);
+  static Color success500 = const Color(0xff3AFF43);
+  static Color success700 = const Color(0xff25A32B);
+
+  //black&white
+  static Color white = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff121212);
+
+  //grayscale
+  static Color grayScale50 = const Color(0xffC1C1C1);
+  static Color grayScale100 = const Color(0xffC1C1C1);
+  static Color grayScale200 = const Color(0xffB2B2B2);
+  static Color grayScale300 = const Color(0xff9F9F9F);
+  static Color grayScale400 = const Color(0xff878787);
+  static Color grayScale500 = const Color(0xff696969);
+  static Color grayScale600 = const Color(0xff545454);
+  static Color grayScale700 = const Color(0xff434343);
+  static Color grayScale800 = const Color(0xff363636);
+  static Color grayScale900 = const Color(0xff2B2B2B);
+
+  //main
+  static Color main50 = const Color(0xffFEED89);
+  static Color main100 = const Color(0xffFEE9A8);
+  static Color main200 = const Color(0xffFEE792);
+  static Color main300 = const Color(0xffFEDD77);
+  static Color main400 = const Color(0xffFED555);
+  static Color main500 = const Color(0xffFECB2B);
+  static Color main600 = const Color(0xffCBA222);
+  static Color main700 = const Color(0xffA2821B);
+  static Color main800 = const Color(0xff826816);
+  static Color main900 = const Color(0xff685312);
+
+  //main2
+  static Color main250 = const Color(0xffB4B6C4);
+  static Color main2100 = const Color(0xffA1A4B5);
+  static Color main2200 = const Color(0xff8A8DA3);
+  static Color main2300 = const Color(0xff6D718C);
+  static Color main2400 = const Color(0xff494D6F);
+  static Color main2500 = const Color(0xff1B214B);
+  static Color main2600 = const Color(0xff161A3C);
+  static Color main2700 = const Color(0xff121530);
+  static Color main2800 = const Color(0xff0E1126);
+  static Color main2900 = const Color(0xff0B0E1E);
+
+}
