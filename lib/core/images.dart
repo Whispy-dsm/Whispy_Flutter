a7a53d8f088b009e6e423d531ffb3c26dd1febed
taejun <ptj01036251@gmail.com>
2025-06-13 14:08:37 +0900

    이미지 추가

--- lib/core/images.dart
+++ lib/core/images.dart
@@ -4,4 +4,8 @@ class Images {
   static String whispyIcon = "assets/images/icons/icon.png";
 
   static String whispyText = "assets/images/whispy_text.png";
+
+  static String introduction1 = "assets/images/introductions/introduction1.png";
+  static String introduction2 = "assets/images/introductions/introduction2.png";
+  static String introduction3 = "assets/images/introductions/introduction3.png";
 }
